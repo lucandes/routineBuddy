51725fca2d2c85e5c58f0b1650c9feacb99faa9e
Lucas Fernandes <lucasfern99@gmail.com>
2024-10-24 02:19:48 -0300

    Categories and activities colors updated
    Changes:
    - Productivity category removed and added two new categories: work, study
    - Activities colors are now defined by the selected category
    - Work rate percentage added to statistics
    - New activity button replaced by simply clicking in the planner bar

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const blackScreen = document.querySelector("#black_screen");
 const avaliableElem = document.querySelector("#avaliable span");
 const occupiedElem = document.querySelector("#occupied span");
-const categoryIconList = ["fa-clock", "fa-bed", "fa-gamepad", "fa-clipboard-question"];
+const categoryIconList = ["fa-clock", "fa-book", "fa-bed", "fa-gamepad", "fa-clipboard-question"];
+const colorPickerList = ["#c51f1f", "#c58a1f", "#9cc51f", "#1fc571", "#1fb6c5", "#1f43c5", "#6e1fc5", "#c51f8a", "#c51f3e", "#212121"];
 // new activity window
 const NEWACTV = document.querySelector("#new_activity");
 const name_NEWACTV = document.querySelector("#new_activity #name");
-const color_NEWACTV = document.querySelector("#new_activity #color");
 const start_NEWACTV = document.querySelector("#new_activity #start");
 const end_NEWACTV = document.querySelector("#new_activity #end");
 const categorySelector = document.querySelector("#new_activity #category #selector");
@@ -22,22 +22,10 @@ const emptyMessage_MNGR = document.querySelector("#manager #empty_message");
 const listField_MNGR = document.querySelector("#manager #list");
 const activityList_MNGR = document.querySelector("#manager #list ul");
 const statisticsField_MNGR = document.querySelector("#manager #statistics");
-const usedTimePercentage_MNGR = document.querySelector("#manager #statistics #progress_value");
+const usedTimePercentage_MNGR = document.querySelector("#manager #statistics #occupied #progress_value");
 const usedTimeProgress_MNGR = document.querySelector("#manager #statistics #occupied .circular_progress");
-
-//// DEBUG, REMOVE LATER
-const testButton = document.querySelector("#teste");
-testButton.style.position = "absolute";
-testButton.style.right = "0";
-testButton.style.top = "0";
-testButton.style.opacity = "10%";
-testButton.onclick = () => {
-  let newAct = new Activity("Dormir", "white", "23:00", "06:30", 1);
-  activityList.push(newAct);
-  console.log("New activity created: "+newAct.name);
-  testButton.style.display = "none";
-  updateRoutineBar();
-}
+const workRatePercentage_MNGR = document.querySelector("#manager #statistics #productivity #progress_value");
+const workRateProgress_MNGR = document.querySelector("#manager #statistics #productivity .circular_progress");
 
 function addNewActivity(){
   if (name_NEWACTV.value == ""){
@@ -51,7 +39,7 @@ function addNewActivity(){
     return;
   }
 
-  let newAct = new Activity(name_NEWACTV.value, color_NEWACTV.value, start_NEWACTV.value, end_NEWACTV.value, selectedCategory_NEWACTV);
+  let newAct = new Activity(name_NEWACTV.value, selectedColor_NEWACTV, start_NEWACTV.value, end_NEWACTV.value, selectedCategory_NEWACTV);
   let conflictFound = false;
 
   for (let i=0; i<activityList.length; i++){
@@ -77,7 +65,6 @@ function toggleAddActivityTab(){
   blackScreen.classList.toggle("show")
   NEWACTV.classList.toggle("open");
   name_NEWACTV.value = '';
-  color_NEWACTV.value = '#000000';
   start_NEWACTV.value = "";
   end_NEWACTV.value = "";
   selectNACategory(0);
@@ -86,6 +73,7 @@ function toggleAddActivityTab(){
 function updateRoutineBar(){
   plannerBar.innerHTML = '';
   occupiedMinutes = 0;
+  workRateMinutes = 0;
   avaliableMinutes = 1440;
   
   activityList.forEach((activityObj, index) => {
@@ -137,8 +125,7 @@ function updateRoutineBar(){
       let endArray = (activityObj.end.split(':')).map(Number);
 
       let elapsedTimeInMinutes = (endArray[0] - startArray[0]) * 60;
-      elapsedTimeInMinutes -= startArray[1];
-      elapsedTimeInMinutes += endArray[0];
+      elapsedTimeInMinutes += endArray[1] - startArray[1];
 
       /* regra de três pra saber a porcentagem, fck the translation */
       let percentage = (elapsedTimeInMinutes * 100) / 1440; // 1440 is the total minutes in 24h
@@ -160,6 +147,7 @@ function updateRoutineBar(){
     }
 
     occupiedMinutes += (activityObj.duration[0] * 60) + activityObj.duration[1];
+    if (activityObj.category == 0 || activityObj.category == 1) workRateMinutes += (activityObj.duration[0] * 60) + activityObj.duration[1];
     console.log(occupiedMinutes);
   })
   
@@ -200,6 +188,8 @@ function updateManager(){
   
   let usedTime = parseInt((occupiedMinutes * 100) / 1440);
   updateCircularProgress(usedTime, usedTimePercentage_MNGR, usedTimeProgress_MNGR);
+  let productiveTime = parseInt((workRateMinutes * 100) / 1440);
+  updateCircularProgress(productiveTime, workRatePercentage_MNGR, workRateProgress_MNGR);
 
   // avaliableMinutes -= occupiedMinutes;
   // avaliableTime.push(parseInt(avaliableMinutes / 60));
@@ -230,19 +220,30 @@ function mouseOverReaction(actionType, elem1, elem2){
 function selectNACategory(index){
   categoryItems[selectedCategory_NEWACTV].classList.toggle("selected");
   categoryItems[index].classList.toggle("selected");
-  if (index == 0){
+  
+  if (index == 0){ // work
     selectedCategory_NEWACTV = 0;
+    selectedColor_NEWACTV = "#9cc51f";
     categorySelector.style.left = "0";
-  } else if (index == 1){
+  } else if (index == 1){ // study
     selectedCategory_NEWACTV = 1;
-    categorySelector.style.left = "25%";
-  } else if (index == 2){
+    selectedColor_NEWACTV = "#ffa500";
+    categorySelector.style.left = "20%";
+  } else if (index == 2){ // rest
     selectedCategory_NEWACTV = 2;
-    categorySelector.style.left = "50%";
-  } else if (index == 3){
+    selectedColor_NEWACTV = "#4c4e81";
+    categorySelector.style.left = "40%";
+  } else if (index == 3){ // relax
     selectedCategory_NEWACTV = 3;
-    categorySelector.style.left = "75%";
+    selectedColor_NEWACTV = "#1fc5bd";
+    categorySelector.style.left = "60%";
+  } else if (index == 4){ // other
+    selectedCategory_NEWACTV = 4;
+    selectedColor_NEWACTV = "#ff00f8";
+    categorySelector.style.left = "80%";
   }
+
+  categorySelector.style.backgroundColor = selectedColor_NEWACTV;
 }
 
 function updateCircularProgress(percentValue, percentDisplay, circularProgressElem){
@@ -312,7 +313,7 @@ class Activity{
 
 /* ELEMENTS BEHAVIOUR */
 add_NEWACTV.onclick = addNewActivity;
-addNewActivityButton.onclick = toggleAddActivityTab;
+plannerBar.onclick = toggleAddActivityTab;
 cancel_NEWACTV.onclick = toggleAddActivityTab;
 Array.from(categoryItems).forEach((elem, index) => {elem.onclick = () => {selectNACategory(index)}})
 
@@ -323,4 +324,19 @@ let occupiedMinutes = 0;
 let occupiedTime = []
 let workRateMinutes = 0;
 let activityList = []
-let selectedCategory_NEWACTV = 0;
+let selectedCategory_NEWACTV = 0; // work, study, rest, relax, other
+let selectedColor_NEWACTV = null;
+
+/* ON START */
+document.addEventListener('DOMContentLoaded', () => {
+  let textLoadProgress = 0;
+  let textContent = "Click here to add a new activity";
+  let plannerBarTextLoad = setInterval(() => {
+    if (textLoadProgress <= textContent.length){
+      plannerBar.innerHTML = textContent.substring(0,textLoadProgress);
+      textLoadProgress++;
+      return;
+    }
+    clearInterval(plannerBarTextLoad);
+  }, 80);
+});
