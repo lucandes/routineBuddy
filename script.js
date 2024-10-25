4ed3b59ef3edf0900b63093ae8058d3a73061021
Lucas Fernandes <lucasfern99@gmail.com>
2024-10-25 02:23:05 -0300

    Added delete and edit functions
    Changes:
    - Edit and delete now working
    - JS better variable names
    - Hour indicator and planner bar style changes

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ const blackScreen = document.querySelector("#black_screen");
 const avaliableElem = document.querySelector("#avaliable span");
 const occupiedElem = document.querySelector("#occupied span");
 const categoryIconList = ["fa-clock", "fa-book", "fa-bed", "fa-gamepad", "fa-clipboard-question"];
-const colorPickerList = ["#c51f1f", "#c58a1f", "#9cc51f", "#1fc571", "#1fb6c5", "#1f43c5", "#6e1fc5", "#c51f8a", "#c51f3e", "#212121"];
-// new activity window
-const NEWACTV = document.querySelector("#new_activity");
-const name_NEWACTV = document.querySelector("#new_activity #name");
-const start_NEWACTV = document.querySelector("#new_activity #start");
-const end_NEWACTV = document.querySelector("#new_activity #end");
-const categorySelector = document.querySelector("#new_activity #category #selector");
-const categoryItems = document.querySelectorAll("#new_activity #category .item");
-const cancel_NEWACTV = document.querySelector("#new_activity #cancel");
-const add_NEWACTV = document.querySelector("#new_activity #add");
+const colorPickerList = ["#9cc51f", "#ffa500", "#4c4e81", "#1fc5bd", "#ff00f8"];
+// activity settings window
+const window_ACTVSET = document.querySelector("#activity_settings");
+const name_ACTVSET = document.querySelector("#activity_settings #name");
+const start_ACTVSET = document.querySelector("#activity_settings #start");
+const end_ACTVSET = document.querySelector("#activity_settings #end");
+const categorySelector = document.querySelector("#activity_settings #category #selector");
+const categoryItems = document.querySelectorAll("#activity_settings #category .item");
+const cancelButton_ACTVSET = document.querySelector("#activity_settings #cancel");
+const addButton_ACTVSET = document.querySelector("#activity_settings #add");
+const editButton_ACTVSET = document.querySelector("#activity_settings #edit");
 // routine display
 const plannerBar = document.querySelector("#bar_field #planner_bar");
-const addNewActivityButton = document.querySelector("#new_activity_button");
-  // manager
-const MNGR = document.querySelector("#manager");
+const addNewActivityButton = document.querySelector("#activity_settings_button");
+// manager
+const window_MNGR = document.querySelector("#manager");
 const emptyMessage_MNGR = document.querySelector("#manager #empty_message");
 const listField_MNGR = document.querySelector("#manager #list");
 const activityList_MNGR = document.querySelector("#manager #list ul");
@@ -26,24 +27,41 @@ const usedTimePercentage_MNGR = document.querySelector("#manager #statistics #oc
 const usedTimeProgress_MNGR = document.querySelector("#manager #statistics #occupied .circular_progress");
 const workRatePercentage_MNGR = document.querySelector("#manager #statistics #productivity #progress_value");
 const workRateProgress_MNGR = document.querySelector("#manager #statistics #productivity .circular_progress");
+// activity delete window
+const window_ADW = document.querySelector("#activity_delete");
+const titleSpan_ADW = document.querySelector("#activity_delete h2 span");
+const cancelButton_ADW = document.querySelector("#activity_delete #cancel");
+const deleteButton_ADW = document.querySelector("#activity_delete #delete");
 
-function addNewActivity(){
-  if (name_NEWACTV.value == ""){
+// const testebtn = document.querySelector("#teste");
+// testebtn.onclick = () => {
+//   let act1 = new Activity("Dormir", "22:30", "07:30", 2);
+//   let act2 = new Activity("Trabalhar", "08:00", "12:30", 0);
+//   let act3 = new Activity("Estudar", "13:15", "17:40", 1);
+//   let act4 = new Activity("Jogar", "18:00", "21:40", 3);
+//   activityList.push(act1, act2, act3, act4);
+//   updateRoutineBar();
+//   testebtn.style.display = "none";
+// }
+
+function checkForActivityErrors(activityObj){
+  if (activityObj.name == ""){
     displayError("The name field cannot be empty");
-    return;
-  } else if (start_NEWACTV.value == "" || end_NEWACTV.value == ""){
+    return true;
+  } else if (activityObj.start == "" || activityObj.end == ""){
     displayError("Please enter the start time and end time of the activity");
-    return;
-  } else if (start_NEWACTV.value == end_NEWACTV.value){
+    return true;
+  } else if (activityObj.start == activityObj.end){
     displayError("The start time and end time cannot be the same");
-    return;
+    return true;
   }
-
-  let newAct = new Activity(name_NEWACTV.value, selectedColor_NEWACTV, start_NEWACTV.value, end_NEWACTV.value, selectedCategory_NEWACTV);
+  
   let conflictFound = false;
 
   for (let i=0; i<activityList.length; i++){
-    if (activityList[i].verifyConflict(newAct)){
+    if (activityList[i].verifyConflict(activityObj) && activityList[i].id != activityObj.id){
+      console.log("ID COMPAIRSON: "+ activityList[i].id +", "+activityObj.id);
+      
       conflictFound = true;
       console.log("Conflict found with "+activityList[i].name);
       break;
@@ -52,22 +70,31 @@ function addNewActivity(){
 
   if (conflictFound){
     displayError("Found conflicts between the new Activity and a existing one, please verify and try again");
-    return;
+    return true;
   }
 
+  return false;
+}
+
+function addNewActivity(){
+  let newAct = new Activity(name_ACTVSET.value, start_ACTVSET.value, end_ACTVSET.value, selectedCategory_ACTVSET);
+  if (checkForActivityErrors(newAct)) return;
+  
   activityList.push(newAct);
   console.log("New activity created: "+newAct.name);
-  toggleAddActivityTab();
+  closeActivityTab();
   updateRoutineBar();
 }
 
-function toggleAddActivityTab(){
-  blackScreen.classList.toggle("show")
-  NEWACTV.classList.toggle("open");
-  name_NEWACTV.value = '';
-  start_NEWACTV.value = "";
-  end_NEWACTV.value = "";
-  selectNACategory(0);
+function editActivity(activityObjIndex){
+  let newAct = new Activity(name_ACTVSET.value, start_ACTVSET.value, end_ACTVSET.value, selectedCategory_ACTVSET);
+  newAct.id = activityList[activityObjIndex].id;
+  if (checkForActivityErrors(newAct)) return;
+  
+  activityList[activityObjIndex] = newAct;
+  console.log("Activity edited: "+newAct.name);
+  closeActivityTab();
+  updateRoutineBar();
 }
 
 function updateRoutineBar(){
@@ -162,17 +189,30 @@ function updateManager(){
     statisticsField_MNGR.classList.remove("hide");
     emptyMessage_MNGR.classList.add("hide");
 
-    activityList.forEach((item) => {
+    activityList.forEach((item, index) => {
       let newItem = document.createElement('li');
       let spanName = document.createElement('span');
       let spanTime = document.createElement('span');
-      let spanIcon = document.createElement('i');
-      spanIcon.classList.add("fas", categoryIconList[item.category])
+      let categoryIcon = document.createElement('i');
+      let optionsDiv = document.createElement('div');
+      let editIcon  = document.createElement('i');
+      let deleteIcon  = document.createElement('i');
+      
+      editIcon.classList.add("fa-solid", "fa-edit");
+      deleteIcon.classList.add("fa-solid", "fa-trash");
+      optionsDiv.appendChild(editIcon);
+      optionsDiv.appendChild(deleteIcon);
+
+      editIcon.onclick = () => {openEditActivityTab(index)};
+      deleteIcon.onclick = () => {openDeleteActivityTab(index)};
+
+      categoryIcon.classList.add("fas", categoryIconList[item.category])
       spanName.innerHTML = item.name;
       spanTime.innerHTML = item.start+" / "+item.end;
-      newItem.appendChild(spanIcon);
+      newItem.appendChild(categoryIcon);
       newItem.appendChild(spanName);
       newItem.appendChild(spanTime);
+      newItem.appendChild(optionsDiv);
       newItem.style.backgroundColor = item.color;
       activityList_MNGR.appendChild(newItem);
     })
@@ -202,6 +242,50 @@ function updateManager(){
 }
 
 /* MINOR FUNCTIONS */
+function openCreateActivityTab(){
+  blackScreen.classList.add("show")
+  window_ACTVSET.classList.add("show");
+  window_ACTVSET.classList.add("create");
+  name_ACTVSET.value = '';
+  start_ACTVSET.value = "";
+  end_ACTVSET.value = "";
+  selectNACategory(0);
+}
+
+function openEditActivityTab(activityObjIndex){
+  blackScreen.classList.add("show")
+  window_ACTVSET.classList.add("show");
+  window_ACTVSET.classList.add("edit");
+  name_ACTVSET.value = activityList[activityObjIndex].name;
+  start_ACTVSET.value = activityList[activityObjIndex].start;
+  end_ACTVSET.value = activityList[activityObjIndex].end;
+  selectNACategory(activityList[activityObjIndex].category);
+  editButton_ACTVSET.onclick = () => {editActivity(activityObjIndex)}
+}
+
+function closeActivityTab(){
+  blackScreen.classList.remove("show")
+  window_ACTVSET.classList.remove("show");
+  window_ACTVSET.classList.remove("create");
+  window_ACTVSET.classList.remove("edit");
+}
+
+function openDeleteActivityTab(activityObjIndex){
+  blackScreen.classList.add("show");
+  window_ADW.classList.add("show");
+  titleSpan_ADW.innerHTML = activityList[activityObjIndex].name;
+  titleSpan_ADW.style.color = activityList[activityObjIndex].color;
+  deleteButton_ADW.onclick = () => {
+    activityList.splice(activityObjIndex, 1);
+    closeDeleteActivityTab();
+    updateRoutineBar();
+  }
+}
+
+function closeDeleteActivityTab(){
+  blackScreen.classList.remove("show");
+  window_ADW.classList.remove("show");
+}
 
 function displayError(message){
   alert("Error: "+message);
@@ -218,32 +302,32 @@ function mouseOverReaction(actionType, elem1, elem2){
 }
 
 function selectNACategory(index){
-  categoryItems[selectedCategory_NEWACTV].classList.toggle("selected");
+  categoryItems[selectedCategory_ACTVSET].classList.toggle("selected");
   categoryItems[index].classList.toggle("selected");
   
   if (index == 0){ // work
-    selectedCategory_NEWACTV = 0;
-    selectedColor_NEWACTV = "#9cc51f";
+    selectedCategory_ACTVSET = 0;
+    selectedColor_ACTVSET = "#9cc51f";
     categorySelector.style.left = "0";
   } else if (index == 1){ // study
-    selectedCategory_NEWACTV = 1;
-    selectedColor_NEWACTV = "#ffa500";
+    selectedCategory_ACTVSET = 1;
+    selectedColor_ACTVSET = "#ffa500";
     categorySelector.style.left = "20%";
   } else if (index == 2){ // rest
-    selectedCategory_NEWACTV = 2;
-    selectedColor_NEWACTV = "#4c4e81";
+    selectedCategory_ACTVSET = 2;
+    selectedColor_ACTVSET = "#4c4e81";
     categorySelector.style.left = "40%";
   } else if (index == 3){ // relax
-    selectedCategory_NEWACTV = 3;
-    selectedColor_NEWACTV = "#1fc5bd";
+    selectedCategory_ACTVSET = 3;
+    selectedColor_ACTVSET = "#1fc5bd";
     categorySelector.style.left = "60%";
   } else if (index == 4){ // other
-    selectedCategory_NEWACTV = 4;
-    selectedColor_NEWACTV = "#ff00f8";
+    selectedCategory_ACTVSET = 4;
+    selectedColor_ACTVSET = "#ff00f8";
     categorySelector.style.left = "80%";
   }
 
-  categorySelector.style.backgroundColor = selectedColor_NEWACTV;
+  categorySelector.style.backgroundColor = selectedColor_ACTVSET;
 }
 
 function updateCircularProgress(percentValue, percentDisplay, circularProgressElem){
@@ -265,14 +349,17 @@ function updateCircularProgress(percentValue, percentDisplay, circularProgressEl
 }
 
 class Activity{
-  constructor(name, color, start, end, category){
+  static instanceCount = 0;
+
+  constructor(name, start, end, category){
     this.name = name;
-    this.color = color;
     this.start = start;
     this.end = end;    
     this.crossDay = start > end ? true : false;
     this.duration = this.#getDuration();
     this.category = category; // currently 4: productive, rest, relax, other
+    this.color = colorPickerList[category];
+    this.id = Activity.instanceCount++;
   }
 
   verifyConflict(newActivity){
@@ -312,20 +399,22 @@ class Activity{
 } /* Activity class end */
 
 /* ELEMENTS BEHAVIOUR */
-add_NEWACTV.onclick = addNewActivity;
-plannerBar.onclick = toggleAddActivityTab;
-cancel_NEWACTV.onclick = toggleAddActivityTab;
+addButton_ACTVSET.onclick = addNewActivity;
+plannerBar.onclick = openCreateActivityTab;
+cancelButton_ACTVSET.onclick = closeActivityTab;
+cancelButton_ADW.onclick = closeDeleteActivityTab;
 Array.from(categoryItems).forEach((elem, index) => {elem.onclick = () => {selectNACategory(index)}})
 
 /* LETS */
 let avaliableMinutes = 1440;
-let avaliableTime = []
+let avaliableTime = [];
 let occupiedMinutes = 0; 
 let occupiedTime = []
 let workRateMinutes = 0;
-let activityList = []
-let selectedCategory_NEWACTV = 0; // work, study, rest, relax, other
-let selectedColor_NEWACTV = null;
+let activityList = [];
+let selectedCategory_ACTVSET = 0; // work, study, rest, relax, other
+let selectedColor_ACTVSET = null;
+let activityIDCounter = 0;
 
 /* ON START */
 document.addEventListener('DOMContentLoaded', () => {
